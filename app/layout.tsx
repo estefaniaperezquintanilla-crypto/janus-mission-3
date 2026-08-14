b39223cac3d2900b52dd27fd4d7ba3620bd1cc05
Estefanía <estefaniaperezquintanilla@gmail.com>
2026-08-14 18:41:44 -0400

    Restore mission 3 visuals and persistent audio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-import { Mission3AudioProvider } from './components/Mission3AudioProvider';
-
 export default function Mission3Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Mission3AudioProvider>{children}</Mission3AudioProvider>;
+  return children;
 }
